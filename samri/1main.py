f4daefcd56cd389904b2145691454e837d62eac1
samra516 <samrafitsum516@gmail.com>
2026-07-23 19:55:23 +0300

    Add files via upload

---
+++ samri/1main.py
@@ -0,0 +1,68 @@
+greeting=str(input("enter you name:"))
+print("welcome here" ,greeting)
+---------------------------------
+number=int(input("enter your number:"))
+number2=int(input("enter your number:"))
+sum=number+number2
+print("the sum is :" ,sum)
+--------------------------------------
+question=str(input("what is your favorite movie: "))
+print("your favorite movie is : ", question)
+-------------------------------------------
+ Checking=input("enter any word : ")
+ val=len(Checking)
+ if val>=5:
+     print("the word is too long")
+ else:
+     print("the word is too short")
+-----------------------------------
+ age=int(input("Enter your age: "))
+ if age>18:
+     print("your eligible to vote")
+ else:
+     print("your not eligible to vote")
+
+print('Item: Apple \n Price: $1.50')
+--------------------------------
+num=int(input("enter a number"))
+if num%2 ==0:
+    print("this num is even")
+else:
+    print("this num is odd")
+
+-----------------------------------------
+user_input = input("Enter True or False: ")
+bool_value = user_input == "True"
+if bool_value:
+    print("Access Granted")
+else:
+    print("Access Denied")
+-----------------------------------------
+temp=int(input("enter the temperature: "))
+if temp<=0:
+   print("the temperature is low")
+else:
+   print("the temperature is high")
+
+----------------------------------------
+num=int(input("enter the number: "))
+num2=int(input("enter the second number: "))
+if num==num2:
+    print("the numbers are equal")
+elif num>=num2:
+    print("num 1 is greater than num2")
+else:
+    print("num 2 is greater than num1")
+
+
+
+
+
+
+
+
+
+
+
+
+
